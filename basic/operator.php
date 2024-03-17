8e93bd3119f1aabd014b56da9697f5dd508b8a4b
Muhammad Faisal Yudiansah <faisal.yudiansah@62teknologi.com>
2024-03-17 16:47:30 +0700

    feat: operator

---
+++ basic/operator.php
@@ -0,0 +1,21 @@
+<?php
+$a = 10;
+$b = 5;
+$result = $a + $b;
+var_dump($result);
+$result -= 3;
+var_dump($result);
+$resultNegative = -$result;
+var_dump($resultNegative);
+$resultPositive = +$result;
+var_dump($result);
+++$result;
+var_dump($result);
+$result++;
+var_dump($result);
+++$result;
+var_dump($result);
+
+$pangkat = 3;
+$resultPangkat = $pangkat ** $pangkat;
+var_dump($resultPangkat);
