d77622afa638dc24abb0a70ed25aff31d03c6840
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2024-04-24 14:05:18 +0700

    feat: function

--- basic/function.php
+++ basic/function.php
@@ -1,4 +1,18 @@
 <?php 
-$dateToday = date("l, D-M-Y, Timezone, j");
+$dateToday = date("l, D-M-Y, Timezone, j \n");
 echo $dateToday;
+
+function testFunction() {
+    echo "This is a function \n";
+}
+testFunction();
+testFunction();
+
+function functionWithParams($name) {
+    echo "Halo my name is $name \n";
+}
+
+functionWithParams("ronaldo");
+functionWithParams("messi");
+
 ?>
